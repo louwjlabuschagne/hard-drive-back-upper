1c56b89002fbcfa17807c177daec52cc0a4aa86f
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2019-04-24 09:10:07 +0200

    Now bat file will work with a double click - provided ananconda activate.bat exists in C:\ProgramData\Anaconda3\Scripts\activate.bat

--- src/run.bat
+++ src/run.bat
@@ -1,1 +1,3 @@
+CALL "C:\ProgramData\Anaconda3\Scripts\activate.bat"
 python C:\Users\labuschagnel01\Downloads\folder_coppier\main.py -v -s='C:\Users\labuschagnel01\Downloads\folder_coppier\src_dir' -d='D:\dst_dir'
+PAUSE
